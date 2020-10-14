e16982e29332df913f0e5bfad74873a766ef2dc0
Nicolas <nicolas.lanzoni@amlabs.com.br>
2020-10-14 17:04:03 -0300

    added sample crawl

--- app/controller.rb
+++ app/controller.rb
@@ -3,6 +3,19 @@ module App
         def self.hello 
             puts 'hello_world'
         end
+
+        def self.get_random_wiki
+            agent = Mechanize::new
+            page = agent.get 'https://en.wikipedia.org/wiki/Special:random'
+
+            doc = Nokogiri::HTML(agent&.page&.body)
+
+            node = doc.xpath('//*[@id="firstHeading"]')&.first
+
+            return node.text
+        end
     end
+
+    
     
 end
